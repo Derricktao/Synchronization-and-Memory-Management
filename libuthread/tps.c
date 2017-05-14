cade29d03fe474e72519372262c4f01e7b33cdc8
zhening(Sirius) zhang <zznzhang@ucdavis.edu>
2017-05-13 19:17:46 -0700

    added page struct, works with part2 phase2

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -13,15 +13,20 @@
 #include "thread.h"
 #include "tps.h"
 
-#define DEFAULT_SIZE 1
-
+#define DEFAULT_SIZE 10
+#define MAX_NUM_ALLOWED_TO_SHARE 100
 /*
 *	Private Section
 */
 
 typedef struct{
+	void* map;
+	int reference_counter;
+} PAGE;
+
+typedef struct{
 	pthread_t tid;
-	void* map; // size should be TPS_SIZE 4096
+	PAGE* page; // size should be TPS_SIZE 4096
 	int valid; // a boolean to distinguish between used & unused cell in array 
 } TPS;
 
@@ -53,7 +58,7 @@ int expand_tps_array(){
 	for(int i =0; i < tb.size; i++){
 		if (temp[i].valid){
 			tb.tps_array[tb.index].tid = temp[i].tid;
-			tb.tps_array[tb.index].map = temp[i].map;
+			tb.tps_array[tb.index].page->map = temp[i].page->map;
 			tb.tps_array[tb.index].valid = 1;
 			tb.index++;
 		}
@@ -92,7 +97,7 @@ static void segv_handler(int sig, siginfo_t *si, void *context)
      */
     int match = 0;
     for (int i =0; i < tb.index; i++){
-    	if(tb.tps_array[i].map == p_fault){
+    	if(tb.tps_array[i].page->map == p_fault){
     		match = 1;
     		break;
     	}
@@ -107,6 +112,33 @@ static void segv_handler(int sig, siginfo_t *si, void *context)
     /* And transmit the signal again in order to cause the program to crash */
     raise(sig);
 }
+
+/*
+int tps_create_CoW(void)
+{
+	pthread_t current_tid = pthread_self();
+	if (find_target_TPS(current_tid)>=0)
+		return -1; // return -1 if the current thread already has a TPS
+
+	tb.tps_array[tb.index].tid = current_tid;
+	tb.tps_array[tb.index].page->map = NULL;
+
+	if (!tb.tps_array[tb.index].page->map || 
+		tb.tps_array[tb.index].page->map == MAP_FAILED){
+		perror("mmap");
+		printf("map is fucked\n");
+		return -1;
+	}
+
+	tb.tps_array[tb.index].valid = 1;
+
+	tb.index++;
+	if (tb.index == tb.size) // array is full
+		if(expand_tps_array() == -1) // if expand fails (malloc fails)
+			return -1;
+	return 0;
+}
+*/
 
 /*
 *	Public Functions
@@ -131,6 +163,8 @@ int tps_init(int segv)
 	tb.tps_array = malloc(DEFAULT_SIZE*sizeof(TPS));
 	for(int i = 0; i < tb.size; i++){
 		tb.tps_array[i].valid = 0;
+		tb.tps_array[i].page = malloc(sizeof(PAGE));
+		tb.tps_array[i].page->reference_counter = 0;
 	}
 	if (tb.tps_array == NULL)
 		return -1;
@@ -144,7 +178,8 @@ int tps_create(void)
 		return -1; // return -1 if the current thread already has a TPS
 
 	tb.tps_array[tb.index].tid = current_tid;
-	tb.tps_array[tb.index].map = mmap(
+	tb.tps_array[tb.index].page = malloc(sizeof(PAGE));
+	tb.tps_array[tb.index].page->map = mmap(
 		NULL,
 		TPS_SIZE,
 		PROT_NONE,
@@ -153,10 +188,9 @@ int tps_create(void)
 		0
 	);
 
-	if (!tb.tps_array[tb.index].map || 
-		tb.tps_array[tb.index].map == MAP_FAILED){
+	if (!tb.tps_array[tb.index].page->map || 
+		tb.tps_array[tb.index].page->map == MAP_FAILED){
 		perror("mmap");
-		printf("map is fucked\n");
 		return -1;
 	}
 
@@ -175,7 +209,7 @@ int tps_destroy(void)
 	if ((index = find_target_TPS(pthread_self()))==-1)
 		return -1; // Return -1 if current thread doesn't have a TPS.
 	
-	if(munmap(tb.tps_array[index].map,TPS_SIZE)){
+	if(munmap(tb.tps_array[index].page->map,TPS_SIZE)){
 		perror("munmap");
 		exit(EXIT_FAILURE);
 	}
@@ -194,9 +228,9 @@ int tps_read(size_t offset, size_t length, char *buffer)
 	if (TPS_SIZE-offset-length < 0)
 		return -1; // reading operation is out of bound
 
-	mprotect(tb.tps_array[index].map, TPS_SIZE, PROT_READ);
-	memcpy(buffer, tb.tps_array[index].map + offset, length);
-	mprotect(tb.tps_array[index].map, TPS_SIZE, PROT_NONE);
+	mprotect(tb.tps_array[index].page->map, TPS_SIZE, PROT_READ);
+	memcpy(buffer, tb.tps_array[index].page->map + offset, length);
+	mprotect(tb.tps_array[index].page->map, TPS_SIZE, PROT_NONE);
 	return 0;
 }
 
@@ -208,9 +242,25 @@ int tps_write(size_t offset, size_t length, char *buffer)
 	if (TPS_SIZE-offset-length < 0)
 		return -1; // reading operation is out of bound
 
-	mprotect(tb.tps_array[index].map, TPS_SIZE, PROT_WRITE);
-	memcpy(tb.tps_array[index].map + offset, buffer, length);
-	mprotect(tb.tps_array[index].map, TPS_SIZE, PROT_NONE);
+	if (tb.tps_array[index].page->reference_counter > 0){
+		void* old_map = tb.tps_array[index].page->map;
+		tb.tps_array[index].page->map = mmap(
+			NULL,
+			TPS_SIZE,
+			PROT_NONE,
+			MAP_ANONYMOUS|MAP_PRIVATE,
+			-1,
+			0
+		);
+		mprotect(old_map, TPS_SIZE, PROT_READ);
+		memcpy(buffer, old_map, TPS_SIZE);
+		mprotect(old_map, TPS_SIZE, PROT_NONE);
+		tb.tps_array[index].page->reference_counter--;
+	}
+
+	mprotect(tb.tps_array[index].page->map, TPS_SIZE, PROT_WRITE);
+	memcpy(tb.tps_array[index].page->map + offset, buffer, length);
+	mprotect(tb.tps_array[index].page->map, TPS_SIZE, PROT_NONE);
 	return 0;
 }
 
@@ -223,19 +273,24 @@ int tps_clone(pthread_t tid)
 	if ((src_index = find_target_TPS(tid))==-1)
 		return -1; // Return -1 if target thread doesn't have a TPS
 
+	//tps_create_CoW();
+
 	tps_create();
 
 	sink_index = find_target_TPS(pthread_self());
 
-	mprotect(tb.tps_array[sink_index].map, TPS_SIZE, PROT_WRITE);
-	mprotect(tb.tps_array[src_index].map, TPS_SIZE, PROT_READ);
+	//tb.tps_array[sink_index].page->map = tb.tps_array[src_index].page->map;
+
+	mprotect(tb.tps_array[sink_index].page->map, TPS_SIZE, PROT_WRITE);
+	mprotect(tb.tps_array[src_index].page->map, TPS_SIZE, PROT_READ);
 	memcpy(
-		tb.tps_array[sink_index].map,
-		tb.tps_array[src_index].map,
+		tb.tps_array[sink_index].page->map,
+		tb.tps_array[src_index].page->map,
 		TPS_SIZE
 	);
-	mprotect(tb.tps_array[sink_index].map, TPS_SIZE, PROT_NONE);
-	mprotect(tb.tps_array[src_index].map, TPS_SIZE, PROT_NONE);
+	mprotect(tb.tps_array[sink_index].page->map, TPS_SIZE, PROT_NONE);
+	mprotect(tb.tps_array[src_index].page->map, TPS_SIZE, PROT_NONE);
+
 	return 0;
 }
 
